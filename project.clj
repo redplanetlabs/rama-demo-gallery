bb061da22cc2972ef6ad26066d30ec1dc23dbd89
Nathan Marz <nathan@redplanetlabs.com>
2023-10-14 07:01:31 -1000

    Rest API Clojure example

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject com.rpl/rama-demo-gallery "1.0.0-SNAPSHOT"
   :source-paths ["src/main/clj"]
   :test-paths ["src/test/clj"]
-  :dependencies [[com.rpl/rama-helpers "0.9.1"]]
+  :dependencies [[com.rpl/rama-helpers "0.9.1"]
+                 [org.asynchttpclient/async-http-client "2.12.3"]]
   :repositories [["releases" {:id "maven-releases"
                               :url "https://nexus.redplanetlabs.com/repository/maven-public-releases"}]]
 

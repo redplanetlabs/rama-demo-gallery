a97a3692a46879ae4d7be87a596c93e848e5c2f9
Nathan Marz <nathan@redplanetlabs.com>
2024-06-25 14:06:08 -1000

    fix logging

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :source-paths ["src/main/clj"]
   :test-paths ["src/test/clj"]
   :dependencies [[com.rpl/rama-helpers "0.9.1"]
+                 [org.apache.logging.log4j/log4j-slf4j18-impl "2.16.0"]
                  [org.asynchttpclient/async-http-client "2.12.3"]]
   :repositories [["releases" {:id "maven-releases"
                               :url "https://nexus.redplanetlabs.com/repository/maven-public-releases"}]]
 
   :profiles {:dev {:resource-paths ["src/test/resources/"]}
-             :provided {:dependencies [[com.rpl/rama "0.11.11"]]}}
+             :provided {:dependencies [[com.rpl/rama "0.16.1"]]}}
   )

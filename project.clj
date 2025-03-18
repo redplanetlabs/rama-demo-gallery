e1c6f2c0bdaaf48270eb5312103cab5a1c82cf99
Nathan Marz <nathan@redplanetlabs.com>
2025-03-18 07:15:06 -1000

    update to rama 1.0.0

--- project.clj
+++ project.clj
@@ -8,5 +8,5 @@
                               :url "https://nexus.redplanetlabs.com/repository/maven-public-releases"}]]
 
   :profiles {:dev {:resource-paths ["src/test/resources/"]}
-             :provided {:dependencies [[com.rpl/rama "0.22.0"]]}}
+             :provided {:dependencies [[com.rpl/rama "1.0.0"]]}}
   )

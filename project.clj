f229c97ac3eb7edba74774c7663d8a2228793372
Nathan Marz <nathan@redplanetlabs.com>
2025-03-18 07:16:44 -1000

    update deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.rpl/rama-demo-gallery "1.0.0-SNAPSHOT"
   :source-paths ["src/main/clj"]
   :test-paths ["src/test/clj"]
-  :dependencies [[com.rpl/rama-helpers "0.9.1"]
+  :dependencies [[com.rpl/rama-helpers "0.10.0"]
                  [org.apache.logging.log4j/log4j-slf4j18-impl "2.16.0"]
                  [org.asynchttpclient/async-http-client "2.12.3"]]
   :repositories [["releases" {:id "maven-releases"

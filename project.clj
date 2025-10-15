8603e7749c0a7378461e972c5a6328539dff3c83
Nathan Marz <nathan@redplanetlabs.com>
2025-10-15 07:12:56 -1000

    bump Rama version

--- project.clj
+++ project.clj
@@ -8,5 +8,5 @@
                               :url "https://nexus.redplanetlabs.com/repository/maven-public-releases"}]]
 
   :profiles {:dev {:resource-paths ["src/test/resources/"]}
-             :provided {:dependencies [[com.rpl/rama "1.0.0"]]}}
+             :provided {:dependencies [[com.rpl/rama "1.2.0"]]}}
   )
